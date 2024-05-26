b8813170bf891624352b615bb6023be3454d841c
Joseph Groot kormelink <josephgrootkormelink@Josephs-MBP.home>
2024-05-26 14:14:19 +0200

    v1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='aih-django',
+    name='aih_django',
     version='0.1',
     packages=find_packages(),
     description='A collection of Django utilities',
