bf6c94f41f6fc607836e48a9295426304b755418
Joseph Groot kormelink <josephgrootkormelink@Josephs-MBP.home>
2024-05-26 14:04:22 +0200

    v1

---
+++ setup.py
@@ -0,0 +1,24 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='aih-django',
+    version='0.1',
+    packages=find_packages(),
+    description='A collection of Django utilities',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Joseph',
+    author_email='joseph@aiheroes.io',
+    url='https://github.com/yourusername/aih-django-utils',
+    install_requires=[
+        'Django>=3.2',  # Specify the Django version compatibility
+    ],
+    classifiers=[
+        'Framework :: Django',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
